11aaa24ea997470d0b9375fd1ebee00f09a8e106
NikolaMomcilovic96 <nikolamomca96@gmail.com>
2021-10-17 20:16:26 +0200

    Code corrections in MainFragment

--- app/src/main/java/com/raywenderlich/nbaplayers/PlayerDetailActivity.kt
+++ app/src/main/java/com/raywenderlich/nbaplayers/PlayerDetailActivity.kt
@@ -5,19 +5,26 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.view.WindowCompat
-import org.w3c.dom.Text
 
 class PlayerDetailActivity : AppCompatActivity() {
 
-    /*private lateinit var binding: PlayerDetailActivityBinding*/
+    private lateinit var binding: PlayerDetailActivityBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_detail)
 
+        binding = PlayerDetailActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
+        showPlayerDetails()
+    }
+
+    private fun showPlayerDetails(){
         val playerName = intent.getStringExtra("title")
         val playerDesc = intent.getStringExtra("desc")
         val playerImage = intent.getIntExtra("image", 0)
