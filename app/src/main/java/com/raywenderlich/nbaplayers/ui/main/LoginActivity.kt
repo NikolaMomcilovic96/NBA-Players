b887e9816b30080d0aacd4ba2eb735649c24cc7d
NikolaMomcilovic96 <nikolamomca96@gmail.com>
2021-10-18 14:44:57 +0200

    Finished login validation

--- app/src/main/java/com/raywenderlich/nbaplayers/ui/main/LoginActivity.kt
+++ app/src/main/java/com/raywenderlich/nbaplayers/ui/main/LoginActivity.kt
@@ -1,10 +1,14 @@
 package com.raywenderlich.nbaplayers.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.raywenderlich.nbaplayers.MainActivity
 import com.raywenderlich.nbaplayers.R
 import com.raywenderlich.nbaplayers.databinding.ActivityLoginBinding
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
 
@@ -19,11 +23,37 @@ class LoginActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.loginButton.setOnClickListener {
-            if (binding.userNametEditText.text.toString() == "Nikola" && binding.passwordEditText.text.toString() == "Momcilovic"){
-                Toast.makeText(this, "Uspeh", Toast.LENGTH_SHORT).show()
+            inputCheck()
+        }
+    }
+
+    private fun inputCheck() {
+        if (binding.userNameEditText.text?.toString()
+                .equals("") || binding.passwordEditText.text?.toString().equals("")
+        ) {
+            Toast.makeText(this, R.string.empty_fields, Toast.LENGTH_SHORT).show()
+        } else if (binding.userNameEditText.text?.length!! < 4) {
+            Toast.makeText(this, R.string.short_username, Toast.LENGTH_SHORT).show()
+        } else if (!passwordCheck(binding.passwordEditText.text.toString())) {
+            Toast.makeText(this, R.string.password_format, Toast.LENGTH_SHORT).show()
+        } else {
+            if (binding.userNameEditText.text.toString() == "Nikola" && binding.passwordEditText.text.toString() == "Momcilovic96") {
+                startActivity(Intent(this, MainActivity::class.java))
+            } else if (binding.userNameEditText.text.toString() == "Nikola" && binding.passwordEditText.text.toString() != "Momcilovic96") {
+                binding.passwordEditText.text?.clear()
+                Toast.makeText(this, R.string.wrong_password, Toast.LENGTH_SHORT).show()
+            } else if (binding.userNameEditText.text.toString() != "Nikola" && binding.passwordEditText.text.toString() == "Momcilovic96") {
+                binding.userNameEditText.text?.clear()
+                Toast.makeText(this, R.string.wrong_username, Toast.LENGTH_SHORT).show()
             }else{
-                Toast.makeText(this, "Greska", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, R.string.login_error, Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun passwordCheck(pass: String): Boolean {
+        val regex: Pattern = Pattern.compile("""^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$""")
+        val text: Matcher = regex.matcher(pass)
+        return text.matches()
     }
 }
